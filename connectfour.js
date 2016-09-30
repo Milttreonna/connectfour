a2d82651ec85b27ade13a53f1a6dd173e31adbb3
James Sibert <jsibert@basecampcodingacademy.org>
2016-09-30 13:53:24 -0500

    added first js

--- connectfour.js
+++ connectfour.js
@@ -2,3 +2,17 @@
 // change player. if click creates 4 in a row, player wins
 
 // if whole game is filled, game over
+
+var game = {
+  whose_turn: "red";
+  columns: [[]];
+
+  drop: function (column_i) {
+    // drop a chip in the col
+    // next turn
+    // hide full column
+    // ??
+  }
+}
+
+game.drop(2)
