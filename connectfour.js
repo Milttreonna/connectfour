450e7765e4ed4553fbb3d96bce0be91ce3c0289f
James Sibert <jsibert@basecampcodingacademy.org>
2016-09-30 14:25:05 -0500

    added columns and notes

--- connectfour.js
+++ connectfour.js
@@ -4,15 +4,21 @@
 // if whole game is filled, game over
 
 var game = {
-  whose_turn: "red";
-  columns: [[]];
+  whose_turn: P1;
+  columns: [["red", null, null, null, null, null],
+            [null, null, null, null, null, null],
+            [null, null, null, null, null, null],
+            [null, null, null, null, null, null],
+            [null, null, null, null, null, null],
+            [null, null, null, null, null, null],
+            [null, null, null, null, null, null]];
 
   drop: function (column_i) {
-    // drop a chip in the col
+    // drop a chip in the col column_i
     // next turn
     // hide full column
     // ??
   }
 }
 
-game.drop(2)
+game.drop(2) // 2 is the third column
