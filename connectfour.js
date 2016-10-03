5ac5c66346269a34a4d4aa277c0a72d4a9ca7274
James Sibert <jsibert@basecampcodingacademy.org>
2016-10-03 15:06:02 -0500

    added to function

--- connectfour.js
+++ connectfour.js
@@ -4,7 +4,7 @@
 
 var game = {
   whose_turn: P1;
-  columns: [["red", null, null, null, null, null],
+  columns: [[null, null, null, null, null, null],
             [null, null, null, null, null, null],
             [null, null, null, null, null, null],
             [null, null, null, null, null, null],
@@ -12,31 +12,21 @@ var game = {
             [null, null, null, null, null, null],
             [null, null, null, null, null, null]];
 
-  dropP1: function (column_i) {
-    for in
-    document.getElementsByTagName('tr')
-    column_i = "red";
-    whose_turn = P2;
-  }
-  dropP2: function (column_i) {
+  drop: function (column_i) {
     // what is the lowest empty row in columns[column_i]
     // update columns[column_i][lowest] to be filled
     // find the td/tr that corresponds to columns[column_i][lowest]
     // update it to be filled
-    var td_to_change = document.getElementsByTagName('tr')[???].getElementsByTagName("td")[colu];
-    // // column_i = "blue";
-    // whose_turn = P1;
+     // column_i = "blue";
+    var td_to_change = document.getElementsByTagName('tr')[???].getElementsByTagName("td")[column_i];
+    if (whose_turn = P1) {
+      td_to_change.src = "img/red.png";
+      whose_turn = P2;
+    } else {
+      td_to_change.src = "img/blue.png";
+      whose_turn = P1;
+    }
   }
 }
 
 game.dropP1(2) // 2 is the third column
-
-
-// function changeImage() {
-//
-//        if (document.getElementById("imgClickAndChange1").src.endsWith("img/empty.png"))
-//        {
-//            document.getElementById("imgClickAndChange1").src = "img/red.png";
-//        }
-//
-//    };
