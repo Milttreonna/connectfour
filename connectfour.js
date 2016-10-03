4625c5fe7293f8b1fca3749f66985cb33f28762e
James Sibert <jsibert@basecampcodingacademy.org>
2016-10-03 15:40:15 -0500

    fixed more errors, gives game over if all spaces are filled

--- connectfour.js
+++ connectfour.js
@@ -1,5 +1,6 @@
 // onclick: if all spaces are taken: do nothing. else: drop to lowest position,
 // change player. if click creates 4 in a row, player wins
+
 var game = {
   whose_turn: "P1",
   columns: [[null, null, null, null, null, null],
@@ -11,11 +12,15 @@ var game = {
             [null, null, null, null, null, null]],
 
   drop: function (column_i) {
+    // what is the lowest empty row in columns[column_i]
+    // update columns[column_i][lowest] to be filled
+    // find the td/tr that corresponds to columns[column_i][lowest]
+    // update it to be filled
     var count = 0;
     for (var i = 0; i < this.columns.length; i++) {
       for (var j = 0; j < this.columns[i].length; j++) {
         if (this.columns[i][j] !== null) {
-          count += 1
+          count += null
         }
       }
     }
@@ -23,22 +28,18 @@ var game = {
       document.getElementById('name').innerText = "Game Over";
       return 0;
     }
-    // what is the lowest empty row in columns[column_i]
-    // update columns[column_i][lowest] to be filled
-    // find the td/tr that corresponds to columns[column_i][lowest]
-    // update it to be filled
-    if (whose_turn === "P1") {
-      columns[column_i] = "red";
+    if (this.whose_turn === "P1") {
+      this.columns[column_i] = "red";
     } else {
-      columns[column_i] = "blue";
+      this.columns[column_i] = "blue";
     }
     var td_to_change = document.getElementsByTagName('tr')[0].getElementsByTagName("td")[column_i];
-    if (whose_turn === "P1") {
+    if (this.whose_turn === "P1") {
       td_to_change.getElementsByTagName('img').src = "img/red.png";
-      whose_turn = "P2";
+      this.whose_turn = "P2";
     } else {
       td_to_change.getElementsByTagName('img').src = "img/blue.png";
-      whose_turn = "P1";
+      this.whose_turn = "P1";
     }
   }
 }
