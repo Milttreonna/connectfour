a9511bf4cb2ed77de332aa11d443f34041af9819
James Sibert <jsibert@basecampcodingacademy.org>
2016-10-03 16:12:42 -0500

    game kinda works, doesnt check for connect 4 yet

--- connectfour.js
+++ connectfour.js
@@ -1,5 +1,5 @@
-// onclick: if all spaces are taken: do nothing. else: drop to lowest position,
-// change player. if click creates 4 in a row, player wins
+// onclick: if all spaces in column are taken: do nothing.
+// if click creates 4 in a row, player wins
 
 var game = {
   whose_turn: "P1",
@@ -12,33 +12,31 @@ var game = {
             [null, null, null, null, null, null]],
 
   drop: function (column_i) {
-    // what is the lowest empty row in columns[column_i]
-    // update columns[column_i][lowest] to be filled
-    // find the td/tr that corresponds to columns[column_i][lowest]
-    // update it to be filled
     var count = 0;
     for (var i = 0; i < this.columns.length; i++) {
       for (var j = 0; j < this.columns[i].length; j++) {
         if (this.columns[i][j] !== null) {
-          count += null
+          count += 1;
         }
       }
     }
+    console.log(count);
     if (count === 42) {
       document.getElementById('name').innerText = "Game Over";
       return 0;
     }
+    var lowest = this.columns[column_i].indexOf(null);
     if (this.whose_turn === "P1") {
-      this.columns[column_i] = "red";
+      this.columns[column_i][lowest] = "red";
     } else {
-      this.columns[column_i] = "blue";
+      this.columns[column_i][lowest] = "blue";
     }
-    var td_to_change = document.getElementsByTagName('tr')[0].getElementsByTagName("td")[column_i];
+    var td_to_change = document.getElementsByClassName('connect4_row')[this.columns[column_i].length -1 - lowest].getElementsByTagName("img")[column_i];
     if (this.whose_turn === "P1") {
-      td_to_change.getElementsByTagName('img').src = "img/red.png";
+      td_to_change.src = "img/red.png";
       this.whose_turn = "P2";
     } else {
-      td_to_change.getElementsByTagName('img').src = "img/blue.png";
+      td_to_change.src = "img/blue.png";
       this.whose_turn = "P1";
     }
   }
