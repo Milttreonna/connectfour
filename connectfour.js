ca39a8863a20a825818727c029f64a9e511e6d68
James Sibert <jsibert@basecampcodingacademy.org>
2016-10-03 14:58:30 -0500

    added lots of js

--- connectfour.js
+++ connectfour.js
@@ -1,52 +1,42 @@
 // onclick: if all spaces are taken: do nothing. else: drop to lowest position,
 // change player. if click creates 4 in a row, player wins
-
 // if whole game is filled, game over
 
-// var game = {
-//   whose_turn: P1;
-//   columns: [["red", null, null, null, null, null],
-//             [null, null, null, null, null, null],
-//             [null, null, null, null, null, null],
-//             [null, null, null, null, null, null],
-//             [null, null, null, null, null, null],
-//             [null, null, null, null, null, null],
-//             [null, null, null, null, null, null]];
+var game = {
+  whose_turn: P1;
+  columns: [["red", null, null, null, null, null],
+            [null, null, null, null, null, null],
+            [null, null, null, null, null, null],
+            [null, null, null, null, null, null],
+            [null, null, null, null, null, null],
+            [null, null, null, null, null, null],
+            [null, null, null, null, null, null]];
+
+  dropP1: function (column_i) {
+    for in
+    document.getElementsByTagName('tr')
+    column_i = "red";
+    whose_turn = P2;
+  }
+  dropP2: function (column_i) {
+    // what is the lowest empty row in columns[column_i]
+    // update columns[column_i][lowest] to be filled
+    // find the td/tr that corresponds to columns[column_i][lowest]
+    // update it to be filled
+    var td_to_change = document.getElementsByTagName('tr')[???].getElementsByTagName("td")[colu];
+    // // column_i = "blue";
+    // whose_turn = P1;
+  }
+}
+
+game.dropP1(2) // 2 is the third column
+
+
+// function changeImage() {
 //
-//   drop: function (column_i) {
-//     // drop a chip in the col column_i
-//     // next turn
-//     // hide full column
-//     // ??
-//   }
-// }
+//        if (document.getElementById("imgClickAndChange1").src.endsWith("img/empty.png"))
+//        {
+//            document.getElementById("imgClickAndChange1").src = "img/red.png";
+//        }
 //
-// game.drop(2) // 2 is the third column
-
-function changeImage() {
-
-       if (document.getElementById("imgClickAndChange1").src.endsWith("img/empty.png"))
-       {
-           document.getElementById("imgClickAndChange1").src = "img/red.png";
-       }
-
-   };
-
-// x.src.endsWith("img/empty.png")
-
-
-
-// {
-//      $('#btn').click(function() {
-//          // change the state of the "#idd"
-//        $('#idd').toggle(800, function() {
-//          // change the button text according to the state of the "#idd"
-//          if ($('#idd').is(':visible')) {
-//            $('#btn').attr('img/red.png', this.href);  // Show Less.. button
-//                  } else {
-//            $('#btn').attr('img/red.png', this.href);  //Learn More.. button
-//
-//          }
-//        });
-//      });
-//    });
+//    };
